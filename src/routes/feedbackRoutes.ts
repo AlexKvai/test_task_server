ee1c562690d4fbe270c9270d337f191a903f257f
alexkvai <alexp-2004@mail.ru>
2025-01-11 18:17:39 +0700

    Добавил роуты для feedback

--- src/routes/feedbackRoutes.ts
+++ src/routes/feedbackRoutes.ts
@@ -1,10 +1,26 @@
 import { Router } from 'express'
+import {
+	createFeedback,
+	deleteFeedback,
+	getAllFeedbacks,
+	updateFeedback,
+} from '../controllers/FeedbackController'
+
+// Middleware для аутентификации
+import authenticate from '../middlewares/authenticate'
 
 const router = Router()
 
-// Пример маршрута для CRUD операций с фидбэками
-router.get('/', (req, res) => {
-	res.send('Get all feedback posts')
-})
+// Пример маршрута для получения всех фидбэков
+router.get('/', getAllFeedbacks)
+
+// Пример маршрута для создания нового фидбэка, с аутентификацией
+router.post('/', authenticate, createFeedback)
+
+// Пример маршрута для обновления фидбэка, с аутентификацией
+router.put('/:id', authenticate, updateFeedback)
+
+// Пример маршрута для удаления фидбэка, с аутентификацией
+router.delete('/:id', authenticate, deleteFeedback)
 
 export default router
