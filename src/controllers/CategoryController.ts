fdc7bd12ab7331d393d81808522ed81704bb6982
alexkvai <alexp-2004@mail.ru>
2025-01-11 18:15:18 +0700

    Добавил проверку на уникальность в контролере

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -5,6 +5,16 @@ const createCategory = async (req: Request, res: Response) => {
 	try {
 		const { name } = req.body
 
+		// Проверяем, есть ли уже такая категория в базе
+		const existingCategory = await prisma.category.findUnique({
+			where: { name }, // Уникальное поле для поиска
+		})
+
+		if (existingCategory) {
+			return res.status(400).json({ message: 'Category already exists' })
+		}
+
+		// Создание категории, если нет дублирующейся записи
 		const category = await prisma.category.create({
 			data: { name },
 		})
