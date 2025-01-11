5510545745879fc7c0493ab3461387a00e2817f1
alexkvai <alexp-2004@mail.ru>
2025-01-11 21:32:21 +0700

    Реализовано получение по id и всех статусов

--- src/controllers/StatusController.ts
+++ src/controllers/StatusController.ts
@@ -1,6 +1,43 @@
 import { Request, Response } from 'express'
 import prisma from '../prismaClient'
 
+const getStatusById = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params
+
+		const status = await prisma.status.findUnique({
+			where: { id: parseInt(id, 10) },
+			select: {
+				id: true,
+				name: true,
+			},
+		})
+
+		if (!status) {
+			return res.status(404).json({ message: 'Status not found' })
+		}
+
+		res.status(200).json(status)
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error', error })
+	}
+}
+
+const getAllStatuses = async (req: Request, res: Response) => {
+	try {
+		const statuses = await prisma.status.findMany({
+			select: {
+				id: true,
+				name: true,
+			},
+		})
+
+		res.status(200).json(statuses)
+	} catch (error) {
+		res.status(500).json({ message: 'Internal server error', error })
+	}
+}
+
 const createStatus = async (req: Request, res: Response) => {
 	try {
 		const { name } = req.body
@@ -25,4 +62,4 @@ const createStatus = async (req: Request, res: Response) => {
 	}
 }
 
-export { createStatus }
+export { createStatus, getAllStatuses, getStatusById }
