5f6b7fa6141e4dd134005b0c91b971105eccd519
alexkvai <alexp-2004@mail.ru>
2025-01-11 21:32:46 +0700

    Добавлены новые роуты и функции обработки из контроллера

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express'
-import { createCategory } from '../controllers/CategoryController'
+import {
+	createCategory,
+	getAllCategories,
+	getCategoryById,
+} from '../controllers/CategoryController'
 const router = Router()
 
 // Пример маршрута для получения категорий
 router.post('/create', createCategory)
+
+router.get('/:id', getCategoryById)
+
+router.get('/', getAllCategories)
 
 export default router
--- src/routes/statusRoutes.ts
+++ src/routes/statusRoutes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
-import { createStatus } from '../controllers/StatusController'
+import {
+	createStatus,
+	getAllStatuses,
+	getStatusById,
+} from '../controllers/StatusController'
 
 const router = Router()
 
 // Пример маршрута для получения статусов
 router.post('/create', createStatus)
+
+router.get('/:id', getStatusById)
+
+router.get('/', getAllStatuses)
 
 export default router
