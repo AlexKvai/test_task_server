4ecbca48eadc180d3ee859a9af742a3128f89e62
alexkvai <alexp-2004@mail.ru>
2025-01-11 22:13:09 +0700

    Добавил фильтрацию и по типу сортировки. Базовая версия готова

--- src/controllers/FeedbackController.ts
+++ src/controllers/FeedbackController.ts
@@ -26,21 +26,47 @@ const createFeedback = async (req: any, res: Response) => {
 
 const getAllFeedbacks = async (req: Request, res: Response) => {
 	try {
-		const { category, status, sort, page = 1, limit = 10 } = req.query
+		const { category, status, sort, sortType, page = 1, limit = 10 } = req.query
 
+		// Фильтры
 		const filters: any = {}
 		if (category) filters.categoryId = Number(category)
 		if (status) filters.statusId = Number(status)
 
+		// Пропуск и лимит для пагинации
 		const skip = (Number(page) - 1) * Number(limit)
+
+		// Сортировка
+		let orderBy: any = {}
+		if (sort) {
+			if (typeof sort !== 'string') throw new Error('Sort must be a string')
+			const sortFields = sort.split(',') // Разделение по запятой
+			orderBy = sortFields.map((field: string) => ({
+				[field]: sortType === 'desc' ? 'desc' : 'asc', // направление сортировки
+			}))
+		} else {
+			orderBy = { createdAt: 'desc' } // по умолчанию сортировка по дате создания
+		}
+
+		// Запрос к базе данных
 		const feedbacks = await prisma.feedback.findMany({
 			where: filters,
-			orderBy:
-				sort === 'upvotes'
-					? { upvotes: { _count: 'desc' } }
-					: { createdAt: 'desc' },
 			skip,
 			take: Number(limit),
+			orderBy,
+			select: {
+				id: true,
+				title: true,
+				description: true,
+				category: {
+					select: { name: true },
+				},
+				status: {
+					select: { name: true },
+				},
+				votesCount: true,
+				createdAt: true,
+			},
 		})
 
 		res.json(feedbacks)
